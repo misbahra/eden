afbc5472e01a2e7b59943cb11a08dce7d1831d8e
misbahra <misbahra@gmail.com>
2021-03-09 23:12:31 +0400

    url

--- edenVictims/urls.py
+++ edenVictims/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('/', views.home, name='home'),
     path('home', views.home, name='home'),
     path('uploaddata', views.upload_data, name='upload_data'),
     path('viewdata', views.view_data, name='view_data'),
