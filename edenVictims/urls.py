13999d796697bafc245b49d94f0034469e46c0c7
misbahra <misbahra@gmail.com>
2021-03-09 23:37:13 +0400

    procfile

--- edenVictims/urls.py
+++ edenVictims/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/', views.home, name='home'),
     path('home', views.home, name='home'),
     path('uploaddata', views.upload_data, name='upload_data'),
     path('viewdata', views.view_data, name='view_data'),
